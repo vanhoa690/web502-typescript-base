1a977f9d979f0429bb5cfdd3a62523bf039c7806
vanhoa690 <vanhoa690@gmail.com>
2026-02-06 19:25:50 +0700

    tutor

--- src/pages/Add.tsx
+++ src/pages/Add.tsx
@@ -1,34 +1,117 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import { useParams } from "react-router-dom";
+import z from "zod";
+
+const validate = z.object({
+  title: z.string().min(1),
+  poster: z.string().min(1),
+  duration: z.number().min(1),
+  category: z.string().min(1),
+});
+
 function AddPage() {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(validate),
+    defaultValues: {
+      duration: 0,
+    },
+  });
+  const { id } = useParams();
+
+  useEffect(() => {
+    const getDetail = async () => {
+      try {
+        const { data } = await axios.get("http://localhost:3000/movies/" + id);
+        reset(data);
+      } catch (error) {
+        toast.error("Loi roi");
+      }
+    };
+    if (id) {
+      getDetail();
+    }
+  }, [id]);
+
+  const onSubmit = async (values: any) => {
+    try {
+      if (id) {
+        await axios.put("http://localhost:3000/movies/" + id, values);
+      } else {
+        await axios.post("http://localhost:3000/movies", values);
+      }
+
+      toast.success("vui qua, xong add roi");
+    } catch (error) {
+      toast.error("Loi roi");
+    }
+  };
   return (
     <div className="p-6">
       <h1 className="text-2xl font-semibold mb-6">Thêm mới</h1>
 
-      <form className="space-y-6">
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
         {/* Text input */}
         <div>
           <label htmlFor="text" className="block font-medium mb-1">
-            Text
+            Title
           </label>
           <input
+            {...register("title")}
             type="text"
             id="text"
             className="w-full border rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
+          <span>{errors?.title?.message}</span>
+        </div>
+        <div>
+          <label htmlFor="text" className="block font-medium mb-1">
+            Poster
+          </label>
+          <input
+            {...register("poster")}
+            type="text"
+            id="text"
+            className="w-full border rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+          <span>{errors?.poster?.message}</span>
         </div>
 
+        <div>
+          <label htmlFor="text" className="block font-medium mb-1">
+            Duration
+          </label>
+          <input
+            {...register("duration", { valueAsNumber: true })}
+            type="number"
+            id="text"
+            className="w-full border rounded-lg px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+          <span>{errors?.duration?.message}</span>
+        </div>
         {/* Select */}
         <div>
           <label htmlFor="selectOption" className="block font-medium mb-1">
             Select - option
           </label>
           <select
+            {...register("category")}
             id="selectOption"
             className="w-full border rounded-lg px-3 py-2 bg-white focus:outline-none focus:ring-2 focus:ring-blue-500"
           >
-            <option value="1">One</option>
+            <option value="Hành động">Hành động</option>
             <option value="2">Two</option>
             <option value="3">Three</option>
           </select>
+          <span>{errors?.category?.message}</span>
         </div>
 
         {/* Submit button */}
--- src/pages/List.tsx
+++ src/pages/List.tsx
@@ -1,4 +1,41 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
+
+type Movie = {
+  id: number;
+  title: string;
+  poster: string;
+  duration: number;
+  category: string;
+};
+
 function ListPage() {
+  const [movies, setMoives] = useState<Movie[]>([]);
+
+  useEffect(() => {
+    const getAll = async () => {
+      try {
+        const { data } = await axios.get("http://localhost:3000/movies");
+        setMoives(data);
+      } catch (error) {
+        toast.error("Loi roi");
+      }
+    };
+    getAll();
+  }, []);
+
+  const handleDetele = async (id: number) => {
+    try {
+      if (confirm("Co muon xoa khong")) {
+        await axios.delete(`http://localhost:3000/movies/${id}`);
+        setMoives(movies.filter((item) => item.id !== id));
+        toast.success("Xoa dc roi");
+      }
+    } catch (error) {
+      toast.error("Loi roi");
+    }
+  };
   return (
     <div className="p-6">
       <h1 className="text-2xl font-semibold mb-6">Danh sách</h1>
@@ -21,12 +58,23 @@ function ListPage() {
           </thead>
 
           <tbody>
-            <tr className="hover:bg-gray-50">
-              <td className="px-4 py-2 border border-gray-300">1</td>
-              <td className="px-4 py-2 border border-gray-300">Mark</td>
-              <td className="px-4 py-2 border border-gray-300">Description</td>
-              <td className="px-4 py-2 border border-gray-300">Edit</td>
-            </tr>
+            {movies.map((item) => (
+              <tr className="hover:bg-gray-50">
+                <td className="px-4 py-2 border border-gray-300">{item.id}</td>
+                <td className="px-4 py-2 border border-gray-300">
+                  {item.title}
+                </td>
+                <td className="px-4 py-2 border border-gray-300">
+                  Description
+                </td>
+                <td className="px-4 py-2 border border-gray-300">
+                  <button onClick={() => handleDetele(item.id)}>Delelte</button>
+                  <button className="mx-2">
+                    <a href={`/edit/${item.id}`}>Edit</a>
+                  </button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
